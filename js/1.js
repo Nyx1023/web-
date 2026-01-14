00d0bf811a147a8c0581bfb8b2152289a967a86a
Nyx1023 <2604072866@qq.com>
2026-01-14 13:05:06 +0800

    Add files via upload
    css和js

---
+++ js/1.js
@@ -0,0 +1,29 @@
+const recommends=document.querySelectorAll('.recommend')
+
+recommends.forEach((recommend) => {
+    const title = recommend.querySelector('.title');
+    const h4 = title.querySelector('h4');
+    const img = recommend.querySelector('img');
+    
+    recommend.addEventListener('mouseenter', function() {
+        
+        // 显示渐变方块
+        title.style.bottom = '0';
+        title.style.opacity = '1';
+		
+        setTimeout(() => {
+            h4.style.transform = 'translateY(0)';
+        }, 100);
+    });
+	
+	recommend.addEventListener('mouseleave', function() {
+        
+        // 隐藏渐变方块
+        title.style.bottom = '-100%';
+        title.style.opacity = '0';
+        
+        // 重置位置
+        h4.style.transform = 'translateY(20px)';
+        
+    });
+});
